4ea85949ab163aa880aff708203c3749226de679
AkhmedovValikhan <ahmedoolo@mail.ru>
2015-05-08 17:04:22 +0500

    Test fot NumberExpression

---
+++ Calc/Calc.Core.Tests/ExpressionTests/NumericExpressionTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Calc.Core.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calc.Core.Tests.ExpressionTests
+{
+    [TestClass]
+    public class NumberExpressionTest
+    {
+        [TestMethod]
+        public void EvaluateTest()
+        {
+            var num = "225,155";
+            var expr = new NumberExpression(num);
+            Assert.AreEqual(225.155, expr.Evaluate(), "Ошибка в вычилсении NumberExpression");
+        }
+    }
+}
