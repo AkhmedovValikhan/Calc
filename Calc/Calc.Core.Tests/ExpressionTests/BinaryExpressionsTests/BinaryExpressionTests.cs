953d8c48fddc0e52a8d98f3443b1817f05211961
AkhmedovValikhan <ahmedoolo@mail.ru>
2015-05-08 17:01:21 +0500

    Tests for BinaryExpressions

---
+++ Calc/Calc.Core.Tests/ExpressionTests/BinaryExpressionsTests/BinaryExpressionTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Calc.Core.Expressions;
+using Calc.Core.Expressions.BinaryExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calc.Core.Tests.ExpressionTests.BinaryExpressionsTests
+{
+    [TestClass]
+    public class BinaryExpressionTests
+    {
+        [TestMethod]
+        public void EvaluationTest()
+        {
+            var operand = new NumberExpression("10");
+            var addition = new AdditionBinaryExpression
+            {
+                LeftOperand = operand,
+                RightOperand = operand
+            };
+
+            var subraction = new SubtractionBinaryExpression
+            {
+                LeftOperand = operand,
+                RightOperand = operand
+            };
+
+            var division = new DivisionBinaryExpression
+            {
+                LeftOperand = operand,
+                RightOperand = operand
+            };
+
+            var multiplication = new MultiplicationBinaryExpression
+            {
+                LeftOperand = operand,
+                RightOperand = operand
+            };
+            Assert.AreEqual(20, addition.Evaluate(), "Ошибка в вычилсении AdditionBinaryExpression");
+            Assert.AreEqual(0, subraction.Evaluate(), "Ошибка в вычилсении SubtractionBinaryExpression");
+            Assert.AreEqual(1, division.Evaluate(), "Ошибка в вычилсении DivisionBinaryExpression");
+            Assert.AreEqual(100, multiplication.Evaluate(), "Ошибка в вычилсении MultiplicationBinaryExpression");
+        }
+    }
+}
