50d26bb56014a27095f35236df08cb37f5018244
AkhmedovValikhan <ahmedoolo@mail.ru>
2015-05-10 13:37:32 +0500

    Refactor ExpressionParser, now CalculatorTests are passing

--- Calc/Calc.Core/Expressions/Parsers/ExpressionParser.cs
+++ Calc/Calc.Core/Expressions/Parsers/ExpressionParser.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using Calc.Core.Expressions.BinaryExpressions;
 
@@ -10,85 +11,112 @@ namespace Calc.Core.Expressions.Parsers
 {
     public class ExpressionParser : IExpressionParser
     {
+        public ExpressionFactory Factory { get; set; }
+        public int HighestPriority { get; set; }
+        public List<IExpression> ExpressionList { get; set; } 
+
+        public ExpressionParser()
+        {
+            Factory = new ExpressionFactory();
+            RegisterBinaryExpressions();
+        }
+
+        private void RegisterBinaryExpressions()
+        {
+            Factory.RegisterBinaryExpression("+", typeof(AdditionBinaryExpression));
+            Factory.RegisterBinaryExpression("-", typeof(SubtractionBinaryExpression));
+            Factory.RegisterBinaryExpression("*", typeof(MultiplicationBinaryExpression));
+            Factory.RegisterBinaryExpression("/", typeof(DivisionBinaryExpression));
+        }
+
         public double ParseAndEvaluate(string expression)
         {
             expression = expression.Replace(" ", "");
 
-            var highestPriority = 2; //will be fix
-            var operandList = new List<IExpression>();
-            var operationList = new List<IBinaryExpression>();
-            var nesting = 0;
+            var highestPriority = 2;
             if (!AnalyzeBrackets(expression))
                 throw new Exception("Unbalanced brackets");
+
+            ExpressionList = Parse(expression);
+            return Evaluate();
+
+
+
+        }
+
+        public List<IExpression> Parse(string expression)
+        {
+            var nesting = 0;
+            var defaultHighestPriority = 10;//will be fix
             var i = 0;
+            var exprList = new List<IExpression>();
 
             while (i < expression.Length)
             {
                 if (Char.IsDigit(expression[i]))
                 {
-                    operandList.Add(ParseNumber(expression, ref i));
-                    continue;
-                }
-                if (BinaryExpressionParser.ContainsOperation(expression[i]))
-                {
-                    var operCh = expression[i];
-                    var operation = BinaryExpressionParser.Parse(operCh);
-                    operation.Priority += nesting;
-                    operationList.Add(operation);
-                    i++;
+                    exprList.Add(ParseNumber(expression, ref i));
                     continue;
                 }
                 //Увлеичиваем уровень вложенность(nesting), тем самым увеличивая приоритет вложенных выражений
                 if (expression[i] == '(')
                 {
-                    nesting += highestPriority;
-                    highestPriority += nesting;
+                    nesting++;
                     i++;
                     continue;
                 }
                 if (expression[i] == ')')
                 {
-                    nesting = highestPriority - nesting;
+                    nesting--;
                     i++;
                     continue;
                 }
+
+                var operation = ParseOperator(expression, ref i);
+                if (operation != null)
+                {
+                    exprList.Add(operation);
+
+                    operation.Priority += nesting * defaultHighestPriority;
+
+                    if (operation.Priority > HighestPriority)
+                        HighestPriority = operation.Priority;
+                    continue;
+                }
+
                 throw new Exception(String.Format("Operator not found: \"{0}\"", expression[i]));
 
             }
+            return exprList;
+        }
 
-            
+        private double Evaluate()
+        {
+            var highestPriority = HighestPriority;
             while (highestPriority > 0)
             {
                 var k = 0;
 
-                if (operationList.Count(expr => expr.Priority == highestPriority) == 0)
+                while (k < ExpressionList.Count)
                 {
-                    k++;
-                    highestPriority--;
-                    continue;
-                }
-               
-                while (k < operationList.Count)
-                {
-                    if (operationList[k].Priority == highestPriority)
+                    if (ExpressionList[k].Priority == highestPriority)
                     {
-                        var binExp = operationList[k];
-                        binExp.LeftOperand = operandList[k];
-                        binExp.RightOperand = operandList[k + 1];
-                        operandList[k] = binExp;
-                        operationList.RemoveAt(k);
-                        operandList.RemoveAt(k + 1);
+                        var binExp = ExpressionList[k] as IBinaryExpression;
+                        binExp.LeftOperand = ExpressionList[k-1];
+                        binExp.RightOperand = ExpressionList[k + 1];
+                        ExpressionList[k - 1] = binExp;
+                        ExpressionList.RemoveRange(k,2);
                     }
                     else k++;
                 }
                 highestPriority--;
             }
 
-            var totalResult = operandList.Single().Evaluate();
+            var totalResult = ExpressionList.Single().Evaluate();
             return totalResult;
-
         }
 
+ 
         public bool AnalyzeBrackets(string expression)
         {
             var bracketsBalance = 0;
@@ -96,7 +124,7 @@ namespace Calc.Core.Expressions.Parsers
             {
                 if (ch == '(')
                 {
-                    bracketsBalance ++;
+                    bracketsBalance++;
                     continue;
                 }
                 if (ch == ')')
@@ -116,6 +144,17 @@ namespace Calc.Core.Expressions.Parsers
                 result.Append(expr[index++]);
             }
             return new NumberExpression(result.ToString());
+        }
+
+        public IBinaryExpression ParseOperator(string expr, ref int index)
+        {
+            var result = new StringBuilder();
+            while (index < expr.Length && expr[index] != '(' && expr[index] != ')' && !Char.IsDigit(expr[index]))
+            {
+                result.Append(expr[index++]);
+            }
+            var restultStr = result.ToString();
+            return Factory.ContainsOperation(restultStr) ? Factory.GetBinary(restultStr) : null;
         }
     }
 }
